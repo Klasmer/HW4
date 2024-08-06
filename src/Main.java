b17bc8b23144cf5cdcb5b0d148438adca162fb80
klasmer <klasmer495@gmail.com>
2024-08-06 18:40:43 +0300

    Домашняя работа 4 Условнй оператор 1 Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("Человеку 18 или больше лет");
+        }
+
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еше не наступил");
+        }
+
     }
 }
