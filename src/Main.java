d83486f2b55a1018d3749f4ad59a021c142d7fbd
klasmer <klasmer495@gmail.com>
2024-08-06 18:58:41 +0300

    Домашняя работа 4 Условнй оператор 1 Задача 3

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,15 @@ public class Main {
             System.out.println(" На улице " + temperature + " градусов, можно идти без шапки");
         }
 
+        System.out.println("Задача 3");
+        int speed = 59;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " , то придется заплатить штраф");
+        }
+        if (speed <= 60) {
+            System.out.println("Если скорость " + speed + " , то можно спокойно ездить");
+        }
+
 
     }
 }
